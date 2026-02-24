e938c5b5d599510976570112e5db12c7a400a87c
1826-keran <kerankatari@gmail.com>
2026-02-24 11:42:46 +0530

    Initialize Flask app with a hello route

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1>DevOps Pipeline Successful!</h1><p>Deployed via GitHub Actions.</p>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
